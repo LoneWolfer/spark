9ed2745c98b8d3db567d6f36a0ac0c99608d072a
LoneWolfer <lone.lancestar@gmail.com>
2017-10-10 14:27:18 +0800

    爬取智联招聘中在杭州所有java实习生的岗位

--- src/com/luner/Main.java
+++ src/com/luner/Main.java
@@ -9,7 +9,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        List<HashMap<String, String>> resultList = analyzeHtml("http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E4%B8%8A%E6%B5%B7&kw=java&sm=0&p=1", "UTF-8");
-        System.out.println(resultList);
+        for (int i = 1; ; i++) {
+            List<HashMap<String, String>> resultList = analyzeHtml("http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E6%9D%AD%E5%B7%9E&kw=java%E5%AE%9E%E4%B9%A0%E7%94%9F&sm=0&isadv=0&sg=8125b58d7cb046daa72f67bc08b5819f&p=" + i, "UTF-8");
+            if (resultList.size() == 1){
+                break;
+            }else {
+                System.out.println(resultList);
+            }
+        }
     }
 }
